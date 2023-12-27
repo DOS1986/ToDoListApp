a761223e2ea634fd7e5f52d8c6d0f681e0f58f58
DOS1986 <34513118+DOS1986@users.noreply.github.com>
2023-12-27 15:28:50 -0500

    Add project files.

---
+++ ToDoListApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using ToDoListApp;
+
+class Program
+{
+    static List<ToDoItem> todoList = new List<ToDoItem>();
+
+    static void Main(string[] args)
+    {
+        bool running = true;
+
+        while (running)
+        {
+            Console.Clear();
+            DisplayTodoList();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Add To-Do Item");
+            Console.WriteLine("2. Mark To-Do Item as Completed");
+            Console.WriteLine("3. Exit");
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    AddTodoItem();
+                    Console.WriteLine("1 Chosen.");
+                    break;
+                case "2":
+                    MarkTodoItem();
+                    Console.WriteLine("2 Chosen.");
+                    break;
+                case "3":
+                    running = false;
+                    Console.WriteLine("3 Chosen.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option, try again.");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+            }
+
+        }
+
+    }
+
+    static void DisplayTodoList()
+    {
+        Console.WriteLine("To-Do List:");
+        for (int i = 0; i < todoList.Count; i++)
+        {
+            var todo = todoList[i];
+            string status = todo.IsCompleted ? "[Completed]" : "[Pending]";
+            Console.WriteLine($"{i + 1}. {todo.Description} {status}");
+        }
+    }
+
+    static void AddTodoItem()
+    {
+        Console.WriteLine("Enter the description of the to-do item:");
+        string description = Console.ReadLine();
+        todoList.Add(new ToDoItem { Description = description });
+    }
+
+    static void MarkTodoItem()
+    {
+        DisplayTodoList();
+        Console.WriteLine("Enter the number of the item to mark as completed:");
+        if (int.TryParse(Console.ReadLine(), out int itemNumber) && itemNumber >= 1 && itemNumber <= todoList.Count)
+        {
+            todoList[itemNumber - 1].IsCompleted = true;
+        }
+        else
+        {
+            Console.WriteLine("Invalid item number.");
+        }
+    }
+}
---
+++ ToDoListApp/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListApp
+{
+    public class ToDoItem
+    {
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
